dc77a2bdb34ac02e63eb476254b754f0874c7fb8
i.molochnikov <i.molochnikov@it-tula.ru>
2023-04-24 22:08:38 +0300

    remove

--- src/infrastructure/error/error.unexpected-status.ts
+++
@@ -1,5 +0,0 @@
-export class ErrorUnexpectedStatus extends Error {
-  constructor(public readonly status: number, url: string, method: 'GET' | 'POST' | 'PUT' | 'PATCH') {
-    super(`Unexpected http status ${status}; url: ${url} ; method: ${method}`);
-  }
-}
--- src/infrastructure/error/error.unexpected.data.ts
+++
@@ -1,1 +0,0 @@
-export class ErrorUnexpectedData extends Error {}
