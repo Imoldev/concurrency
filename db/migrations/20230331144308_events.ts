0f03bd9cb953586d242f0de4e916d10742df9f45
i.molochnikov <i.molochnikov@scloud.ru>
2023-10-31 17:27:33 +0300

    add field

--- db/migrations/20230331144308_events.ts
+++ db/migrations/20230331144308_events.ts
@@ -5,6 +5,7 @@ export async function up(knex: Knex): Promise<Knex.SchemaBuilder> {
     table.collate('utf8_unicode_ci');
     table.bigIncrements('id').primary();
     table.string('type').notNullable();
+    table.datetime('created_on', { precision: 6 }).defaultTo(knex.fn.now(6))
     table.json('payload');
   });
 }
