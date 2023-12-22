fac1566cc223b442031c56712446f52f9d3f0ca1
i.molochnikov <i.molochnikov@scloud.ru>
2023-12-22 10:29:40 +0300

    insert increment

--- db/migrations/20231214143240_events_inrements.ts
+++ db/migrations/20231214143240_events_inrements.ts
@@ -4,7 +4,7 @@ import { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
     return knex.schema.createTable('events_increments', function (table) {
         table.collate('utf8_unicode_ci');
-        table.bigInteger('id').primary();
+        table.bigIncrements('id').primary();
       });
 }
 
--- db/migrations/20231214150503_increment_events_id_trigger.ts
+++ db/migrations/20231214150503_increment_events_id_trigger.ts
@@ -2,8 +2,7 @@ import { Knex } from "knex";
 
 
 export async function up(knex: Knex): Promise<void> {
-    await knex.raw("CREATE TRIGGER increment_events_id BEFORE INSERT ON events FOR EACH ROW UPDATE events_increments SET id = LAST_INSERT_ID(id+1), NEW.id = LAST_INSERT_ID();")
-    return knex.raw("INSERT INTO events_increments VALUES (0);")
+    await knex.raw("CREATE TRIGGER increment_events_id BEFORE INSERT ON events FOR EACH ROW BEGIN INSERT INTO events_increments SET id = DEFAULT; SET NEW.id = LAST_INSERT_ID(); END;")
 }
 
 
